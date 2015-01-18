133026b7a7c7d65aa31658549a904eb89e3bf780
Adam Siefker <siefker@gmail.com>
2015-01-18 06:33:41 -0800

    Hit a problem with this in startElection.
    Test is failing but this is a good check point.

--- raft.js
+++ raft.js
@@ -1,7 +1,27 @@
 var assert = require("assert");
+var log4js = require('log4js');
 
-exports.Raft = function () {
+var logger = log4js.getLogger();
 
+exports.VoteResponse = function(term, granted) {
+    this.term = term;
+    this.granted = granted;
+};
+
+/**
+ * Create a Raft instance
+ * @param id - the id of this instance
+ * @param others {array} - ids of the other Raft instances
+ * @param send {function} - callback that takes 4 parameters: 
+ *      name, map for the request, per success callback, completion callback
+ */
+exports.Raft = function (id, others, send) {
+    // Raft server states
+    this.states = {
+        candidate: "candidate",
+        follower: "follower",
+        leader: "leader"
+    };
     this.State = function() {
         this.currentTerm = 0;
         this.currentLogIndex = 0;
@@ -15,30 +35,28 @@ exports.Raft = function () {
         };
     };
     
-    function VoteResponse(term, granted) {
-        this.term = term;
-        this.granted = granted;
-    }
-
-
-    function requestVote() {
-        logger.info("Requesting vote");
-        state = "candidate";
-        currentTerm += 1;
-        votedFor = id;
-        voteReq = {"term": currentTerm, "candidateId": id, 
-                "lastLogIndex": currentLogIndex, "lastLogTerm":currentTerm};
+    this.startElection = function() {
+        logger.info("Requesting starting election");
+        logger.info("All keys: " + Object.keys(this));
+        this.curState = this.states.candidate;
+        this.state.currentTerm += 1;
+        this.votedFor = id;
+        voteReq = {"term": this.state.currentTerm, 
+                "candidateId": this.id, 
+                "lastLogIndex": this.state.currentLogIndex, 
+                "lastLogTerm":this.state.currentTerm};
         var grantedCount = 0;
-        sendAll("/vote", voteReq, function(vRes) {
+        this.send("/vote", voteReq, function(vRes) {
             if (vRes.granted) {
                 grantedCount +=1;
             } else {
                 // set currentTerm to max seen
-                currentTerm = Math.max(currentTerm, vRes.term);
+                // todo this state needs to kill the current election
+                this.state.currentTerm = Math.max(this.state.currentTerm, vRes.term);
             }
         }, function(){
             logger.info("Complete: " + grantedCount);
-            if (grantedCount > others.length/2) {
+            if (grantedCount > this.others.length/2) {
                 logger.info("Become leader");
                 becomeLeader();
             }
@@ -48,6 +66,21 @@ exports.Raft = function () {
                 electionTimeout = newElectionTimeout();    
             }
         }); 
-    }
+    };
 
+    
+    this.newElectionTimeout = function() {
+        return setTimeout(function(a){
+            logger.info("Election Timeout");
+            a.startElection(); 
+        },
+        1000 + Math.floor(Math.random() * 2000), this);
+    };
+    
+    this.id = id;
+    this.curState= this.states.follower;
+    this.electionTimeout = this.newElectionTimeout();
+    this.state = new this.State();
+    this.send = send;    
+    this.others = others;
 };
--- test/raft.js
+++ test/raft.js
@@ -1,11 +1,16 @@
 var assert = require("assert");
 var raft = require("../raft");
-
+console.log(Object.keys(assert));
+console.log(assert);
 describe('Raft.State', function() {
     describe('#logIsUpToDate', function(){
-        console.log("Foo");
-        console.log(assert);
-        r = new raft.Raft();
+        beforeEach(function(){
+            r = new raft.Raft();
+        });
+        afterEach(function(){
+            clearTimeout(r.electionTimeout); 
+        });
+
         it('Term is always > 0', function() {
             assert.throws(function(){
                 new r.State().logIsUpToDate(0,0);        
@@ -29,3 +34,36 @@ describe('Raft.State', function() {
         });
     });
 });
+
+describe('Raft.ElectionTimeout', function(){
+    it('Starts Election', function(done){
+        this.timeout(3000);
+        r = new raft.Raft(0, [], function(n, d, e, a){done();});
+    });
+});
+
+
+describe('Raft.Election', function(){
+    beforeEach(function(){
+        r = new raft.Raft();
+        // disable timeout to make tests faster
+        clearTimeout(r.electionTimeout); 
+    });
+    it('Election increases term and votes for self', function(done){
+        var r = new raft.Raft(0, [], function(n, d, e, a){
+            assert.equal(1, r.state.currentTerm);
+            assert.equal("candidate", r.curState);
+            assert.equal(0, r.votedFor);
+            done();});
+    });
+    it('Declare winner with majority', function() {
+        var r = new raft.Raft(0, [1,2], function(n, d, e, a) {
+            e(new raft.VoteResponse(d.term, true));
+            e(new raft.VoteResponse(d.term, true));
+            a();
+            assert.equal("leader", r.curState);
+        });
+    });
+});
+
+
